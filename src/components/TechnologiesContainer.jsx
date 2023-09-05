85d69370c6162a9f6e0f4763ed53775532f49eda
Johann Matos Furtado <johannfurtado@gmail.com>
2023-09-05 14:45:53 -0300

    📱: add responsividade mobile

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -6,7 +6,7 @@ import {
     DiReact,
     DiGit,
   } from "react-icons/di"
-  import { SiFlutter, SiLaravel, SiPhp } from "react-icons/si"
+  import { SiFlutter, SiLaravel, SiPhp, SiVuedotjs } from "react-icons/si"
   import "../styles/components/technologiescontainer.sass"
   
   const technologies = [
@@ -15,6 +15,7 @@ import {
     { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
     { id: "mysql", name: "MySQL", icon: <DiMysql /> },
     { id: "react", name: "React", icon: <DiReact /> },
+    { id: "vue", name: "Vue.js", icon: < SiVuedotjs/>},
     { id: "flutter", name: "Flutter", icon: < SiFlutter/>},
     { id: "php", name: "PHP", icon: <SiPhp /> },
     { id: "laravel", name: "Laravel", icon: <SiLaravel /> },
@@ -27,6 +28,7 @@ import {
     js: "Linguagem versátil, dinâmica, amplamente usada em desenvolvimento web, front-end e back-end.",
     mysql: "Banco de dados relacional de código aberto para armazenamento e gerenciamento de dados.",
     react: "Biblioteca JavaScript para criar interfaces de usuário interativas e reutilizáveis.",
+    vue: "Framework JavaScript progressivo para construir interfaces de usuário.",
     flutter: "Framework da Google para desenvolver apps nativos Android e iOS.",
     php: "Linguagem de servidor para criar aplicativos web dinâmicos e interativos.",
     laravel: "Framework PHP de código aberto para desenvolvimento web simplificado.",
