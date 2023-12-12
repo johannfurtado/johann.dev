431980b3b2362033308af198377f65f55a8597f3
Johann Furtado <61987811+johannfurtado@users.noreply.github.com>
2023-12-12 14:34:45 -0300

    Update AboutContainer.jsx

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -13,6 +13,9 @@ const AboutContainer = () => {
         Também atuei como Dev FullStack na Tradio Bank, uma startup no ramo de subadquirentes de meios de pagamentos, onde dei manutenção e desenvolvi funcionalidades novas no ERP da empresa utilizando diariamente React.js e PHP Laravel, para controle de versionamento era utilizado o git e o github e como metodologia ágil Scrum e Kanban, com isso aprimorei ainda mais minha técnicas de programação, lógica e aprendi regras de negócio valiosas do ramo financeiro.
         </p>
         <p>
+        Atualmente trabalho na Zetti Tech, empresa que desenvolve soluções de tecnologia para varejo farmacêutico, abrangendo desde a retaguarda da operação até o consumidor final, aqui estou aprimorando muito minhas técnicas de programação com uma equipe bastante qualificada, além de agregar conhecimento e resolver problemas reais todos os dias.
+        </p>
+        <p>
         Gosto de desenvolver coisas novas e a área de tecnologia e inovação sempre me encantou muito. Almejo desenvolver minhas skills, aprender novas habilidades e crescer profissionalmente em ambientes que prezem pela organização e evolução. Tenho muita facilidade e disposição para aprender e sempre estou buscando conhecimentos que agregam para mim, tanto pessoal como profissionalmente.
         </p>
       </section>
